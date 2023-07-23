42a2bdeaba420bee12dbfa9b577c7613b1aa69c2
Vladimir Semikov <voesem@ya.ru>
2023-07-23 20:06:43 +0300

    реализована основная логика работы программы

--- main.py
+++ main.py
@@ -3,34 +3,32 @@ from utils import *
 
 def main():
     print('Привет! Это программа для поиска вакансий на сайтах HeadHunter и SuperJob')
-    platform_name = input('Выберите платформу:\n1 - HeadHunter\n2 - SuperJob\n')
     search_name = input('Введите поисковый запрос: ')
-    if platform_name == '1':
-        hh = HeadHunterAPI()
-        json_saver = JSONSaver()
-        hh.get_vacancies(search_name)
-        Vacancy.instantiate_from_json()
+    hh = HeadHunterAPI()
+    sj = SuperJobAPI()
+    json_saver = JSONSaver()
+    hh.get_vacancies(search_name)
+    sj.get_vacancies(search_name)
+    Vacancy.instantiate_from_json()
 
-        salary = int(input('Введите начальный порог зарплаты: '))
+    salary = int(input('Введите начальный порог зарплаты: '))
 
-        num = 0
-        filtered_vacancies = []
-        for vacancy in Vacancy.all:
-            if vacancy.salary_from >= salary:
-                filtered_vacancies.append(vacancy.as_dict())
-                num += 1
-                print(
-                    f'{num}. Название вакансии: {vacancy.name}\n'
-                    f'Название компании: {vacancy.employer}\n'
-                    f'Зарплата: от {vacancy.salary_from} до {vacancy.salary_to} рублей.'
-                )
-                print()
+    num = 0
+    filtered_vacancies = []
+    for vacancy in Vacancy.all:
+        if vacancy.salary_from >= salary:
+            filtered_vacancies.append(vacancy.as_dict())
+            num += 1
+            print(
+                f'{num}. Название вакансии: {vacancy.name}\n'
+                f'Название компании: {vacancy.employer}\n'
+                f'Зарплата: от {vacancy.salary_from} до {vacancy.salary_to} рублей.'
+            )
+            print()
 
-        json_saver.add_vacancy(filtered_vacancies)
-    elif platform_name == '2':
-        pass
-    else:
-        print('Неизвестная команда')
+    print('Вакансии сохранены в файл saved_vacancies.json')
+
+    json_saver.add_vacancy(filtered_vacancies)
 
 
 if __name__ == '__main__':
